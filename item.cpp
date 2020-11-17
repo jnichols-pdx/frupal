34cc81126df624ad49a2e3c99830b71ce109b745
Karlrosenberg <74164914+Karlrosenberg@users.noreply.github.com>
2020-11-17 10:29:06 -0800

    Added skeleton of item.h and item.cpp
    The item class will perform actions on the Hero depending on what item the Hero is using.

---
+++ item.cpp
@@ -0,0 +1,25 @@
+//item class functions
+
+
+#include "item.h"
+
+
+
+void item::binocular(Hero * to_edit)
+{
+	
+}
+
+
+
+void item::treasure_chest(Hero * to_edit)
+{
+	
+}
+
+
+
+void item::royal_diamond(Hero * to_edit)
+{
+	
+}
