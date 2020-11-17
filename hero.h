2ed8465c4d315f8f31bb5420940d109b2c8284db
anthonybob <46734229+anthonybob@users.noreply.github.com>
2020-11-16 17:20:31 -0800

    Updated spacing

--- hero.h
+++ hero.h
@@ -1,7 +1,7 @@
 class Hero
 {
   public:
-		Hero(); //Hero constructor
+	Hero(); //Hero constructor
   	Hero(int whif, int nRG); //Hero constructor
   	void modEner(int nRG); //modify energy - adds by default. To lower energy, send in negative number
   	void modWhif(int Whif); //modify whiffles - adds by default. To lower number of whiffles, send in negative number
