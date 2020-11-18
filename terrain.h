3c7b75e631caf24800655827a242bb1cc69698d9
anthonybob <46734229+anthonybob@users.noreply.github.com>
2020-11-18 01:27:05 -0800

    Update terrain.h

--- terrain.h
+++ terrain.h
@@ -11,4 +11,5 @@ class terrain   	//base class for terrains
 		bool display_info(char);   //displays information of the terrain in the menu
 		int get_travel_cost(char);	//returns cost to travel
 		int get_color(char);	//returns color of terrain
+	private:
 };
