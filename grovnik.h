586184598d4fe46ebe163234faac4e5d03a3d5af
Karlrosenberg <74164914+Karlrosenberg@users.noreply.github.com>
2020-11-17 16:05:35 -0800

    Added the skeleton of grovnik
    Feel free to change/add

---
+++ grovnik.h
@@ -0,0 +1,96 @@
+//This file is for managing the classes of the grovniks
+
+#include "frupal.h"
+
+
+//base class for grovniks
+class grovnik
+{
+	public:
+		grovnik();
+		
+		//virtual functions
+		virtual ~grovnik();
+		virtual void display_info() = 0;   //displays information of the grovnik
+		
+	protected:
+	
+	private:
+};
+
+class binocular : public grovnik
+{
+	public:
+		binocular();
+		~binocular();
+		void display_info();
+	protected:
+	
+	private:
+};
+
+class treasure_chest : public grovnik
+{
+	public:
+		treasure_chest();
+		treasure_chest(int amount);
+		~treasure_chest();
+		void display_info();
+	protected:
+	
+	private:	
+};
+
+class royal_diamond : public grovnik
+{
+	public:
+		royal_diamond();
+		~royal_diamond();
+		void display_info();
+	protected:
+	
+	private:	
+};
+
+class obstacle : public grovnik
+{
+	public:
+		obstacle();
+		void display_info();
+	protected:
+	
+	private:	
+};
+
+class tool : public grovnik
+{
+	public:
+		tool();
+		tool(int type);
+		~tool();
+		void display_info();
+	protected:
+	
+	private:	
+};
+
+class food : public grovnik
+{
+	public:
+		food();
+		food(int type);
+		~food();
+		void display_info();
+	protected:
+	
+	private:	
+};
+
+class clue : public grovnik
+{
+	public:
+		void display_info();
+	protected:
+	
+	private:	
+};
