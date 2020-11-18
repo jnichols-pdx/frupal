7f3200b954c7c299e279921d7e51ead33b8e41d8
Anthony Bobrovnikov <anthonybobrovnikov@gmail.com>
2020-11-18 01:04:54 -0800

    fixed spacing

--- terrain.h
+++ terrain.h
@@ -2,13 +2,12 @@
 
 #include "frupal.h"
 
-
 class terrain   	//base class for terrains
 {
-  public:
-    terrain();
+	public:
+		terrain();
 		~terrain();
-    bool display_info(char);   //displays information of the terrain in the menu
-    int get_travel_cost(char);	//returns cost to travel
-    int get_color(char);	//returns color of terrain
+		bool display_info(char);   //displays information of the terrain in the menu
+		int get_travel_cost(char);	//returns cost to travel
+		int get_color(char);	//returns color of terrain
 };
