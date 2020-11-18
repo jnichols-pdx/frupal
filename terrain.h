078f606a7d70fb7aa8baad8b54d07340df255e8e
Anthony Bobrovnikov <anthonybobrovnikov@gmail.com>
2020-11-18 00:59:27 -0800

    changed terrain into one class. added get_color, and get_travel_cost

--- terrain.h
+++ terrain.h
@@ -7,45 +7,8 @@ class terrain   	//base class for terrains
 {
   public:
     terrain();
-		virtual ~terrain();
-    virtual void display_info() = 0;   //displays information of the terrain in the menu
-    int get_travel_cost();	//returns cost to travel
-    int get_color();	//returns color of terrain
-
-  protected:
-    int travel_cost;    //cost of energy for player to travel through 
-		int color; //color of terrain
+		~terrain();
+    bool display_info(char);   //displays information of the terrain in the menu
+    int get_travel_cost(char);	//returns cost to travel
+    int get_color(char);	//returns color of terrain
 };
-
-class meadow: public terrain
-{
-  public:
-		meadow();
-		~meadow();
-    void display_info();
-};
-
-class swamp: public terrain
-{
-  public:
-		swamp();
-		~swamp();
-    void display_info();
-};
-
-class water: public terrain
-{
-  public:
-    water();
-		~water();
-    void display_info();
-};
-
-class wall: public terrain
-{
-  public:
-    wall();
-		~wall();
-    void display_info();
-};
-
